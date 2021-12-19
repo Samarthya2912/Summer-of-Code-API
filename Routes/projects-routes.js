97ba93aeed15441753db31c2c1a03f0367b5d637
Samarthya <samarthya55@gmail.com>
2021-12-19 11:59:11 +0530

    get projects route

--- Routes/projects-routes.js
+++ Routes/projects-routes.js
@@ -2,6 +2,5 @@ const router = require("express").Router();
 const placesControllers = require("../Controllers/projects-controllers");
 
 router.get("/", placesControllers.getProjects);
-router.get("/:pid", placesControllers.getProjectByRepo);
 
 module.exports = router;
