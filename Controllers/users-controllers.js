5bf650ef2099aa6c3b26388db1916b6ea698023f
Samarthya <samarthya55@gmail.com>
2022-01-19 08:24:21 +0530

    changes to user-controllers, added github token to jwt payload

--- Controllers/users-controllers.js
+++ Controllers/users-controllers.js
@@ -5,29 +5,30 @@ const HttpError = require("../Models/http-error");
 const User = require("../Models/user-model");
 
 async function getUser(req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
-    let payload = null;
+  const token = req.headers.authorization.split(" ")[1];
+  let payload = null;
 
-    try {
-        payload = jwt.verify(token, process.env.JWT_SECRET);
-    } catch(err) {
-        return next(new HttpError(err.message, 404));
-    }
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return next(new HttpError(err.message, 404));
+  }
+  console.log("payload", payload);
 
-    let userData = null;
-    try {
-        const response = await axios({
-            method: "get",
-            url: `https://api.github.com/users/${payload.login}`
-        })
-        
-        userData = response.data;
-    } catch(err) {
-        return next(new HttpError(err.message));
-    }
+  let userData = null;
+  try {
+    const response = await axios({
+      method: "get",
+      url: `https://api.github.com/users/${payload.login}`,
+    });
 
-    if(!userData) return next(new HttpError("Invalid user", 404));
-    res.json({ userData });
+    userData = response.data;
+  } catch (err) {
+    return next(new HttpError(err.message));
+  }
+
+  if (!userData) return next(new HttpError("Invalid user", 404));
+  res.json({ userData });
 }
 
 async function authenticateUser(req, res, next) {
@@ -37,48 +38,59 @@ async function authenticateUser(req, res, next) {
       url: "https://github.com/login/oauth/access_token",
       method: "post",
       data: {
-        client_id: "bf778f87e9514dc519ca",
-        client_secret: "5d8913960414065a161b82c3682870f7f8350655",
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
         code: req.body.code,
       },
       headers: { Accept: "application/json" },
     });
 
     access_token = response.data.access_token;
+    console.log("token", access_token);
 
-    if (access_token === null || access_token === undefined) return next(new HttpError("Token not generated.", 404));
-    // else return res.json({ access_token });
+    if (access_token === null || access_token === undefined)
+      return next(new HttpError("Token not generated.", 404));
   } catch (err) {
     return next(new HttpError(err.message, 500));
   }
 
   let userData = null;
   try {
-      const response = await axios({
-          method: "get",
-          url: "https://api.github.com/user",
-          headers: { Authorization: `token ${access_token}` }
-      })
+    const response = await axios({
+      method: "get",
+      url: "https://api.github.com/user",
+      headers: { Authorization: `token ${access_token}` },
+    });
 
-      userData = response.data;
+    userData = response.data;
+    console.log("User data", userData);
 
-      const newUser = new User({
-        id: userData.id,
-        login: userData.login
-      });
+    if (userData === null || userData === undefined)
+      return next(new HttpError("No user found.", 404));
 
-      const identifiedUser = await User.findOne({ id: userData.id, login: userData.login });
+    const newUser = new User({
+      id: userData.id,
+      login: userData.login,
+    });
 
-      if(!identifiedUser) await newUser.save();
+    const identifiedUser = await User.findOne({
+      id: userData.id,
+      login: userData.login,
+    });
 
-      if(userData === null || userData === undefined) {
-          return next(new HttpError("No user found.", 404))
-      } else {
-          const token = jwt.sign({ id: userData.id ,login: userData.login }, process.env.JWT_SECRET, { expiresIn: "1h" });
-          return res.json({ token, userData })
-      }
-  } catch(err) {
-      return next(new HttpError(err.message))
+    if (!identifiedUser) {
+      await newUser.save();
+      console.log("Saving the user as it doesn't exist.");
+    }
+
+    const token = jwt.sign(
+      { id: userData.id, login: userData.login, github_access_token: access_token },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    return res.json({ token, userData });
+  } catch (err) {
+    return next(new HttpError(err.message));
   }
 }
 
